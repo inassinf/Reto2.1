c214acc032768f300db44cb47b33b26c2f9024c3
inassinf <124713361+inassinf@users.noreply.github.com>
2023-02-10 10:15:37 +0100

    Add files via upload

---
+++ reto5.js
@@ -0,0 +1,7 @@
+function suma(x,y,z){console.log(x+y+z);}
+function resta(x,y){console.log(x-y);}
+function multiplicacion(x,y){console.log(x*y);}
+function division(x,y){console.log(x/y);}
+function cuadrado(x){console.log(x*x);}
+
+module.exports = {suma, resta, multiplicacion, division, cuadrado};
